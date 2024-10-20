9b0b0d91f975ece1282ebcb53ad6928dee451135
Henrik Kvamme <henrik@kvamme.xyz>
2024-10-20 08:19:02 -0700

    Add indexing for the attestations.

--- app/api/attest/route.ts
+++ app/api/attest/route.ts
@@ -3,6 +3,7 @@ import {
   SignProtocolClient,
   SpMode,
   EvmChains,
+  IndexService
 } from "@ethsign/sp-sdk";
 import { privateKeyToAccount } from "viem/accounts";
 
@@ -23,17 +24,43 @@ export async function POST(request: Request) {
       account: account,
     });
 
+    // Create an indexing service
+    const indexingService = new IndexService("testnet");
+
+    console.log("userAddress", userAddress);
+    console.log("Checking for existing attestations. LessonId:", lessonId);
+
+    const existingAttestations = await indexingService.queryAttestationList({
+      schemaId: "onchain_evm_80002_0x7c",
+      indexingValue: userAddress,
+      page: 0,
+      mode: "onchain",
+    });
+
+    console.log('Number of existing attestations:', existingAttestations?.rows.length);
+
+    const hasExistingAttestation = (existingAttestations?.rows.length ?? 0) > 0;
+
+    console.log('Has existing attestation:', hasExistingAttestation);
+
+    if (hasExistingAttestation) {
+      return NextResponse.json({ success: false, error: 'Attestation for this lesson already exists' }, { status: 400 });
+    }
+
+    // If no existing attestation, create a new one
     const createAttestationRes = await client.createAttestation({
       schemaId: "0x7c",
-      data: { lesson_id: BigInt(lessonId) },
+      data: {
+        lessonId: `0x${parseInt(lessonId).toString(16).padStart(64, '0')}`,
+      },
       indexingValue: userAddress,
-      recipients: [userAddress], // Set the user as the recipient
+      recipients: [userAddress],
     });
 
     return NextResponse.json({ success: true, attestationId: createAttestationRes.attestationId });
   } catch (error) {
     console.error('Error creating attestation:', error);
     console.error('Error details:', JSON.stringify(error, null, 2));
-    return NextResponse.json({ success: false, error: 'Failed to create attestation', details: error.message }, { status: 500 });
+    return NextResponse.json({ success: false, error: 'Failed to create attestation', details: String(error) }, { status: 500 });
   }
 }
